85410ecbf1e34eb4bccf7122d91020a1a28ec753
20CpsByKarel <87567412+20CpsByKarel@users.noreply.github.com>
2024-09-08 14:02:08 +0200

    Update bannery.js

--- bannery.js
+++ bannery.js
@@ -1,9 +1,21 @@
 document.addEventListener("DOMContentLoaded", function() {
     var carouselItems = document.querySelectorAll(".carousel-item");
+    var language = upgates.language.toLowerCase();  // Převod na malá písmena pro zajištění konzistence
+
+    // Povolené jazyky
+    var supportedLanguages = ["hu", "ro", "pl", "en", "de", "it", "sl", "sk", "fi"];
+    
+    // Nastavení výchozího jazyka na "en" pokud je jazyk nepodporovaný
+    if (!supportedLanguages.includes(language)) {
+        language = "en";
+    }
+
     carouselItems.forEach(function(item, index) {
         item.style.cursor = "pointer";
-                if (index === 0) {
-            item.setAttribute("href", "https://www.henrymorgan.cz/en/action");
+
+        if (index === 0) {
+            // Dynamické nastavení URL na základě jazyka
+            item.setAttribute("href", "https://www.henrymorgan.cz/" + language + "/action");
         }
         else if (index === 1) {
             item.setAttribute("href", "https://t.me/henry_morgan_europe");
