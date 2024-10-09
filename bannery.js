b13feb423c0c0b69a7d2805403e9f483cf609f03
20CpsByKarel <87567412+20CpsByKarel@users.noreply.github.com>
2024-10-09 20:46:58 +0200

    Update bannery.js

--- bannery.js
+++ bannery.js
@@ -15,7 +15,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
         if (index === 0) {
             // Dynamické nastavení URL na základě jazyka
-            item.setAttribute("href", "https://t.me/henry_morgan_europe");
+            item.setAttribute("href", "https://t.me/+iDeOp5VXKVQ4NDhk");
         }
         else if (index === 1) {
             item.setAttribute("href", "https://dsc.gg/henrymorgan");
