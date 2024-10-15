3f653e2b2149a6f52590b1322e419b6a0bafba17
20CpsByKarel <87567412+20CpsByKarel@users.noreply.github.com>
2024-10-15 20:20:24 +0200

    Update bannery.js

--- bannery.js
+++ bannery.js
@@ -1,29 +1,27 @@
-<script>
-    document.addEventListener("DOMContentLoaded", function() {
-        var carouselItems = document.querySelectorAll(".carousel-item");
-        var language = upgates.language.toLowerCase();  // Převod na malá písmena pro zajištění konzistence
+document.addEventListener("DOMContentLoaded", function() {
+    var carouselItems = document.querySelectorAll(".carousel-item");
+    var language = upgates.language.toLowerCase();  // Převod na malá písmena pro zajištění konzistence
 
-        // Povolené jazyky
-        var supportedLanguages = ["hu", "ro", "pl", "en", "de", "it", "sl", "sk", "fi"];
-        
-        // Nastavení výchozího jazyka na "en" pokud je jazyk nepodporovaný
-        if (!supportedLanguages.includes(language)) {
-            language = "en";
-        }
+    // Povolené jazyky
+    var supportedLanguages = ["hu", "ro", "pl", "en", "de", "it", "sl", "sk", "fi"];
+    
+    // Nastavení výchozího jazyka na "en" pokud je jazyk nepodporovaný
+    if (!supportedLanguages.includes(language)) {
+        language = "en";
+    }
 
-        carouselItems.forEach(function(item, index) {
-            item.style.cursor = "pointer"; // Nastavení kurzoru na pointer
+    carouselItems.forEach(function(item, index) {
+        item.style.cursor = "pointer";
 
+        if (index === 0) {
             // Dynamické nastavení URL na základě jazyka
-            if (index === 0) {
-                item.setAttribute("href", "https://www.henrymorgan.cz/" + language + "/action");
-            } 
-            else if (index === 1) {
-                item.setAttribute("href", "https://t.me/+iDeOp5VXKVQ4NDhk");
-            } 
-            else if (index === 2) {
-                item.setAttribute("href", "https://dsc.gg/henrymorgan");
-            }
-        });
+            item.setAttribute("href", "https://www.henrymorgan.cz/" + language + "/action");
+        }
+        else if (index === 1) {
+            item.setAttribute("href", "https://t.me/henry_morgan_europe");
+        }
+        else if (index === 2) {
+            item.setAttribute("href", "https://dsc.gg/henrymorgan");
+        }
     });
-</script>
+});
