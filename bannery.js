a0a4f6275736f8465bda44f1079c25f8ced807a0
Karel Provazník <87567412+20CpsByKarel@users.noreply.github.com>
2024-11-10 14:07:34 +0100

    Update bannery.js

--- bannery.js
+++ bannery.js
@@ -14,9 +14,10 @@ document.addEventListener("DOMContentLoaded", function() {
         item.style.cursor = "pointer";
 
         if (index === 0) {
+            item.setAttribute("href", "https://dsc.gg/henrymorgan");
             // Dynamické nastavení URL na základě jazyka
             //item.setAttribute("href", "https://t.me/henry_morgan_europe");
-            item.setAttribute("href", "https://www.henrymorgan.cz/" + language + "/action");
+            //item.setAttribute("href", "https://www.henrymorgan.cz/" + language + "/action");
         }
         else if (index === 1) {
             //item.setAttribute("href", "https://t.me/+iDeOp5VXKVQ4NDhk");
