1ddb03e62ce31282964f8f8b2139453c525b6447
Karel Provazník <87567412+20CpsByKarel@users.noreply.github.com>
2024-11-11 08:29:10 +0100

    Update bannery.js

--- bannery.js
+++ bannery.js
@@ -14,17 +14,18 @@ document.addEventListener("DOMContentLoaded", function() {
         item.style.cursor = "pointer";
 
         if (index === 0) {
-            item.setAttribute("href", "https://dsc.gg/henrymorgan");
+            //item.setAttribute("href", "https://dsc.gg/henrymorgan");
             // Dynamické nastavení URL na základě jazyka
-            //item.setAttribute("href", "https://t.me/henry_morgan_europe");
+            item.setAttribute("href", "https://t.me/henry_morgan_europe");
             //item.setAttribute("href", "https://www.henrymorgan.cz/" + language + "/action");
         }
         else if (index === 1) {
-            //item.setAttribute("href", "https://t.me/+iDeOp5VXKVQ4NDhk");
-            item.setAttribute("href", "https://www.reddit.com/r/HenryMorgan/s/7QaPccFXJk");
+            item.setAttribute("href", "https://dsc.gg/henrymorgan");
+            //item.setAttribute("href", "https://www.reddit.com/r/HenryMorgan/s/7QaPccFXJk");
         }
         else if (index === 2) {
-            item.setAttribute("href", "https://dsc.gg/henrymorgan");
+            item.setAttribute("href", "https://www.reddit.com/r/HenryMorgan/s/7QaPccFXJk");
+            //item.setAttribute("href", "https://dsc.gg/henrymorgan");
         }
     });
 });
